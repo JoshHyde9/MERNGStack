24f658ce622a8f6c7fc0ffb50a95ca8db4000f81
Josh Hyde <joshhyde@pop-os.localdomain>
2019-07-02 19:30:45 +1000

    Added display of posts

---
+++ client/src/components/PostCard.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Card, Icon, Label, Image, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import moment from "moment";
+
+function PostCard({
+  post: { body, createdAt, id, username, likeCount, commentCount, likes }
+}) {
+  function likePost() {
+    console.log("Post liked!");
+  }
+
+  function commentOnPost() {
+    console.log("Posted comment");
+  }
+  return (
+    <Card fluid>
+      <Card.Content>
+        <Image
+          floated="right"
+          size="mini"
+          src="https://react.semantic-ui.com/images/avatar/large/steve.jpg"
+        />
+        <Card.Header>{username}</Card.Header>
+        <Card.Meta as={Link} to={`/posts/${id}`}>
+          {moment(createdAt).fromNow(true)}
+        </Card.Meta>
+        <Card.Description>{body}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button as="div" labelPosition="right" onClick={likePost}>
+          <Button color="red" basic>
+            <Icon name="heart" />
+          </Button>
+          <Label basic color="red" pointing="left">
+            {likeCount}
+          </Label>
+        </Button>
+        <Button as="div" labelPosition="right" onClick={commentOnPost}>
+          <Button basic color="blue">
+            <Icon name="comments" />
+          </Button>
+          <Label as="a" basic color="blue" pointing="left">
+            {commentCount}
+          </Label>
+        </Button>
+      </Card.Content>
+    </Card>
+  );
+}
+
+export default PostCard;
