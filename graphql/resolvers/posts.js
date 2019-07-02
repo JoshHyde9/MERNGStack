6950d6462dca69a2cbdf181e0564d6ca0fb3b0d6
Josh Hyde <joshhyde@pop-os.localdomain>
2019-07-02 16:38:04 +1000

    Added calculation to like and comment count

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,7 +1,7 @@
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/check-auth");
 
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
 module.exports = {
   Query: {
@@ -55,6 +55,25 @@ module.exports = {
         }
       } catch (err) {
         throw new Error(err);
+      }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find(like => like.username === username)) {
+          post.likes = post.likes.filter(like => like.username !== username);
+        } else {
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString()
+          });
+        }
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError("Post not found!");
       }
     }
   }
