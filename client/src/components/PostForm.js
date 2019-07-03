e3b585a8ab49d8fce237e4c63dc1cc14cbaf61ba
Josh Hyde <joshhyde@pop-os.localdomain>
2019-07-03 11:34:01 +1000

    Added error displaying

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -30,20 +30,30 @@ function PostForm() {
   }
 
   return (
-    <Form onSubmit={onSubmit}>
-      <h2>Create A Post</h2>
-      <Form.Field>
-        <Form.Input
-          placeholder={"What's on your mind, " + user.username + "?"}
-          name="body"
-          onChange={onChange}
-          value={values.body}
-        />
-        <Button type="submit" color="teal">
-          Post
-        </Button>
-      </Form.Field>
-    </Form>
+    <>
+      <Form onSubmit={onSubmit}>
+        <h2>Create A Post</h2>
+        <Form.Field>
+          <Form.Input
+            placeholder={"What's on your mind, " + user.username + "?"}
+            name="body"
+            onChange={onChange}
+            value={values.body}
+            error={error ? true : false}
+          />
+          <Button type="submit" color="teal">
+            Post
+          </Button>
+        </Form.Field>
+      </Form>
+      {error && (
+        <div className="ui error message" style={{ marginBottom: 20 }}>
+          <ul className="list">
+            <li>{error.graphQLErrors[0].message}</li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
 
